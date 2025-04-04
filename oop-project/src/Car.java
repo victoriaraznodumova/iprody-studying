7032c1b0a3d579bd0e97234d577417b2a07833c8
Victoria Raznodumova <raznodumova@gmail.com>
2025-04-05 00:12:19 +0300

    tasks of hw 7 finished

---
+++ oop-project/src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    private void startElectricity(){}
+    private void startCommand(){}
+    private void startFuelSystem(){}
+
+    public void start(){
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+}
---
+++ oop-project/src/sameclass/SameName.java
@@ -0,0 +1,4 @@
+package sameclass;
+
+public class SameName {
+}
