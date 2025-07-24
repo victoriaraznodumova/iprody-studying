4819489dce1ab3a66a9698ca994a73a0c6850ad1
Victoria Raznodumova <raznodumova@gmail.com>
2025-07-24 14:42:39 +0300

    new version of task1

--- algorithms/src/lesson5/task1/Main.java
+++ algorithms/src/lesson5/task1/Main.java
@@ -4,30 +4,37 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Инициализация нового списка: ");
         SinglyLinkedList list1 = new SinglyLinkedList();
         //Инициализация списка
         System.out.println(Arrays.toString(list1.initialize(5)));
         //Добавление в список
         System.out.println(Arrays.toString(list1.add(list1.getArray(), 9)));
         //Получение значения по индексу
-        System.out.println(list1.get(list1.getArray(), 2));
+        try{
+            System.out.println(list1.get(list1.getArray(), 2));
+        }
+        catch (Exception e){
+            System.out.println("Элемент с указанным индексом не найден");
+        }
         //Добавление в список
         System.out.println(Arrays.toString(list1.add(list1.getArray(), 8)));
         System.out.println(Arrays.toString(list1.add(list1.getArray(), 7)));
         System.out.println(Arrays.toString(list1.add(list1.getArray(), 6)));
         System.out.println(Arrays.toString(list1.add(list1.getArray(), 5)));
         System.out.println(Arrays.toString(list1.add(list1.getArray(), 4)));
-        //Получение размера списка
-        System.out.println("Размер списка: " + list1.getSIZE());
+        System.out.println(Arrays.toString(list1.getArray()));
+        //Получение вместимости списка
+        System.out.println("Вместимость списка: " + list1.getCapacity(list1.getArray()));
         //Получение количества элементов в списке
-        System.out.println("Количество элементов в списке: " + list1.getLength(list1.getArray()));
-
+        System.out.println("Количество элементов в списке: " + list1.getSize());
+        System.out.println("\nИнициализация нового списка: ");
         SinglyLinkedList list2 = new SinglyLinkedList();
         //Инициализация списка
         System.out.println(Arrays.toString(list2.initialize(2)));
-        //Получение размера списка
-        System.out.println("Размер списка: " + list1.getSIZE());
+        //Получение вместимости списка
+        System.out.println("Вместимость списка: " + list2.getCapacity(list2.getArray()));
         //Получение количества элементов в списке
-        System.out.println("Количество элементов в списке: " + list2.getLength(list2.getArray()));
+        System.out.println("Количество элементов в списке: " + list2.getSize());
     }
 }
--- algorithms/src/lesson5/task1/SinglyLinkedList.java
+++ algorithms/src/lesson5/task1/SinglyLinkedList.java
@@ -1,42 +1,40 @@
 package lesson5.task1;
 
-import java.util.Random;
+import java.util.Arrays;
 
 public class SinglyLinkedList {
-    private int SIZE = 10;
+    private int size;
     private int[] array;
 
     public int[] getArray() {
         return array;
     }
-    public int[] initialize(int size) {
-        array = new int[size];
-        Random random = new Random();
-        for (int i = 0; i < size; i++) {
-            array[i] = random.nextInt(10);
-        }
-//        for (int element: newList) {
-//            element = random.nextInt(50);
-//        }
+    public int[] initialize(int capacity) {
+        array = new int[capacity];
+        size = 0;
         return array;
     }
-    public int[] add(int[] list, int element){
-        if (list.length >= SIZE) {
-            SIZE = (int) Math.round(SIZE * 1.5 + 1);
+    public int[] add(int[] input, int element){
+        if (size >= input.length) {
+            int newCapacity = (int) Math.round(size * 1.5 + 1);
+            int[] destArray = new int[newCapacity];
+            System.arraycopy(input, 0, destArray, 0, input.length);
+            array = destArray;
         }
-        int[] destArray = new int[list.length + 1];
-        System.arraycopy(list, 0, destArray, 0, list.length);
-        destArray[list.length] = element;
-        array = destArray;
-        return array;
+        array[size] = element;
+        size++;
+        return Arrays.copyOfRange(array, 0, size);
     }
-    public int get(int[] array, int index){
+    public int get(int[] array, int index) throws Exception {
+        if (index >= size) {
+            throw new Exception();
+        }
         return array[index];
     }
-    public int getSIZE() {
-        return SIZE;
+    public int getSize() {
+        return size;
     }
-    public int getLength(int[] array){
+    public int getCapacity(int[] array){
         return array.length;
     }
 }
