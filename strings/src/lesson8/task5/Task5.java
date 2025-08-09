0bbca125aaef444b294715076920c006235a4474
Victoria Raznodumova <raznodumova@gmail.com>
2025-08-09 22:19:21 +0300

    tasks of hw 8 finished

---
+++ strings/src/lesson8/task5/Task5.java
@@ -0,0 +1,9 @@
+package lesson8.task5;
+
+public class Task5 {
+
+    String[] values = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
+    "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi",
+    "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper",
+    "pineapple", "pumpkin", "potato"};
+}
